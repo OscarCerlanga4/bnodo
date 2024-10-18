b02f66b88f5ea1b0a66a4bdf93f5c53c11d1a3b0
OscarCerlanga4 <oscarcerlanga@iesrioarba.es>
2024-10-18 14:19:00 +0200

    first commit

---
+++ bnodo.py
@@ -0,0 +1,25 @@
+#! C:\Users\alumno24\AppData\Local\Microsoft\WindowsApps\python.exe
+
+
+class bnodo:
+    __info = None
+    __de = None
+    __iz = None
+    
+    def __init__(self, info = None):
+        self.__info = info
+        
+    def __str__(self):
+        answ = "("
+        if self.__iz == None:
+            answ += "(.)"
+        answ += str(self.__info)
+        if self.__de == None:
+            answ += "(.)"
+        answ = ")"
+        return answ
+
+if __name__ == "__main__":
+    
+    bn0 = bnodo()
+    print(bn0)
