522ea6dabc33936c77b942719a1c36e27a479959
OscarCerlanga4 <oscarcerlanga@iesrioarba.es>
2024-10-22 10:23:17 +0200

    .

---
+++ bnodo0.py
@@ -0,0 +1,27 @@
+#! C:\Users\alumno24\AppData\Local\Microsoft\WindowsApps\python.exe
+
+class bnodo:
+
+    __info = None
+    __de = None
+    __iz = None
+
+    def __init__(self,info = None) -> None:
+        self.__info = info
+
+
+    def __str__(self) -> str:
+        answ = "("
+        if self.__iz == None:
+            answ += "(.)"
+        answ += str(self.__info)
+        if self.__de == None:
+            answ += "(.)"
+        answ += ")"
+        return answ
+
+if __name__ == "__main__":
+
+    bn0 = bnodo()
+    print(bn0)
+
